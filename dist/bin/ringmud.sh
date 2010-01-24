dd84994f8d8db0332fd830bed2feaf9881ad69c3
ProjectMoon <someguy@someplace.com>
2010-01-24 16:24:03 -0500

    Removed dist

--- dist/bin/ringmud.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-JARDIR=`dirname $0`/jar
-LIBDIR=`dirname $0`/../lib
-#export CLASSPATH=$JARDIR/ring-persistence.jar:$JARDIR/ring-core.jar:$JARDIR/ring-legacy.jar:$JARDIR/ring-commands.jar:$JARDIR/ring-server.jar:$JARDIR/ring-nrapi.jar:$JARDIR/ringmud.jar
-#echo $CLASSPATH
-ENDORSEDDIR=$JARDIR/lib/endorsed
-java -Djava.endorsed.dirs=$ENDORSEDDIR -cp "${LIBDIR}/*:jar/*" ring.main.RingMain $*
