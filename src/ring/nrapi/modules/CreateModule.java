ed8308b1c5d4cb70c4989f26c733edcf18599a4d
ProjectMoon <someguy@someplace.com>
2010-01-25 16:10:04 -0500

    Merged datastore fixes and added new module for clearing database

---
+++ src/ring/nrapi/modules/CreateModule.java
@@ -0,0 +1,38 @@
+package ring.nrapi.modules;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import ring.main.RingModule;
+import ring.nrapi.xml.RingDocument;
+import ring.persistence.ExistDB;
+
+/**
+ * Imports XML documents into the database.
+ * @author projectmoon
+ *
+ */
+public class CreateModule implements RingModule {
+	@Override
+	public void execute(String[] args) {
+		System.out.println("Clearing database...");
+		try {
+			ExistDB db = new ExistDB();
+			db.removeAllResources();
+			db.createRingDatabase();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Done.");
+	}
+
+	@Override
+	public boolean usesDatabase() {
+		return true;
+	}
+	
+}
